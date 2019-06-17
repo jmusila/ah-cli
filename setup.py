a8a6650adcf444424c398ed35d5a6eefbd5c1c64
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-17 09:35:41 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="ah-cli",
+        version="0.1",
+        py_modules=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            "Click",
+            "requests",
+            ],
+        entry_points="""
+        [console_scripts]
+        ah=app:cli
+        """,
+        )
