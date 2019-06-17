f32106477f2cd3cd260a8feea74323e41463005b
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-17 17:19:59 +0300

    added tests

--- app/test_articles.py
+++ app/test_articles.py
@@ -6,6 +6,7 @@ def test_get_single_article():
     runner = CliRunner()
     result = runner.invoke(get, ['new_wangonya'])
     assert result.exit_code == 0
+    assert "new_wangonya" in result.output
     assert "Status code: 200" in result.output
 
 
@@ -13,6 +14,7 @@ def test_get_all_articles():
     runner = CliRunner()
     result = runner.invoke(get_list)
     assert result.exit_code == 0
+    assert "None" in result.output
     assert "Status code: 200" in result.output
 
 
@@ -21,3 +23,11 @@ def test_main():
     res = runner.invoke(main)
     assert res.exit_code == 0
     assert "Simple CLI for consuming Authors Haven App 😍" in res.output
+
+
+def test_get_single_article_invalid_slug():
+    runner = CliRunner()
+    result = runner.invoke(get, ['new_article'])
+    assert result.exit_code == 0
+    assert "Status code: 404" in result.output
+    assert "new_article" in result.output
