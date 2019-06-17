1ee1a9d0519109bb43db1f3a6d5a3155b6e2b2fa
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-17 10:15:28 +0300

    added articles

--- app/articles.py
+++ app/articles.py
@@ -1,8 +1,8 @@
 import sys
 import requests
-import json
 
 from halo import Halo
 
 spinner = Halo(text='Loading', spinner='dots', text_color='magenta')
 url = "https://ah-django-staging.herokuapp.com/api"
+
