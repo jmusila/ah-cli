729037567d1543744d85e3e81f17edfb1ea23857
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-17 09:44:00 +0300

    added setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-        name="ah-cli",
+        name="a-haven",
         version="0.1",
         py_modules=find_packages(),
         include_package_data=True,
